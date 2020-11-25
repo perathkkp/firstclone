ddf65d63706eafcfc71595279d113c4d52f6fbd1
perathkkp <74891014+perathkkp@users.noreply.github.com>
2020-11-25 10:37:47 +0700

    asd

--- scrap_looper/__init__.py
+++ scrap_looper/__init__.py
@@ -7,11 +7,24 @@ import pandas as pd
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     
-    url2 = ['https://tide.pier.or.th/chart/FICBDLPBSMM00303','https://tide.pier.or.th/chart/FIPGRTQ00564','https://tide.pier.or.th/chart/FICBDLPBSMM01382','https://tide.pier.or.th/chart/FICBDLPTCSQ00128']
+    # url2 = ['https://tide.pier.or.th/chart/FICBDLPBSMM00303','https://tide.pier.or.th/chart/FIPGRTQ00564','https://tide.pier.or.th/chart/FICBDLPBSMM01382','https://tide.pier.or.th/chart/FICBDLPTCSQ00128']
+    # url = req.params.get("url")
+    try:
+        req_body = req.get_json()
+        url = req_body.get('url')
+        filename = req_body.get('name')
+    except ValueError:
+        return func.HttpResponse(
+        "No Url in body",
+        status_code=400
+        )
+        
+    url2 =[]
+    url2.append(url)
     xpath = '//*[@id="list-data"]/ul/li[position()>=2]/text()'
     nameXpath = '//*[@id="chart-section"]/div/div/div[1]/div/div[1]/div/div/div/div[1]/span/text()'
     
-    for i in range(len(url2)):7
+    for i in range(len(url2)):
         bruh = Helpers.extract_element(url2[i], xpath)
         autoName = Helpers.extract_element(url2[i],nameXpath)
         np.array(bruh)
@@ -19,13 +32,16 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         #reName = autoName[0].replace("","")
         reName = autoName[0].strip()
         
-        print(changed_out)
-        name = "./Tide Listing/"+autoName[0]+".csv"
-        print(name)
+        # print(changed_out)
+        # name = "./Tide Listing/"+autoName[0]+".csv"
+        name = "./Tide Listing/"+filename+".csv"
+        # print(filename+"AAAOK")
         df = pd.DataFrame(index=changed_out[0:][0:])
-        
+        # df.to_csv(path_or_buf = "./Tide Listing/"+autoName[0]+".csv", header=False)
+        df.to_csv(path_or_buf = "./Tide Listing/"+filename+".csv", header=False)
+   
 
     return func.HttpResponse(
-        "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.", 
+        "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
         status_code=200
     )
